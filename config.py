b765a440e4b51938d2dda51cbf4a7a7f062bd3d6
marudevelop <105143258+marudevelop@users.noreply.github.com>
2025-11-19 15:50:35 +0900

    Update dataset paths in config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import torch
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 DATA_ROOT = 'data/hmdb51/rawframes'
-TRAIN_TXT = 'data/hmdb51/train.txt'
-VAL_TXT = 'data/hmdb51/val.txt'
-MOTION_JSON = 'data/hmdb51/motion_diff.json'
+TRAIN_TXT = 'data/hmdb51/hmdb51_train_split_1_rawframes.txt'
+VAL_TXT = 'data/hmdb51/hmdb51_val_split_1_rawframes.txt'
+MOTION_JSON = 'data/hmdb51/hmdb51_img_diff.json'
 
 NUM_CLASSES = 51
 NUM_SEGMENTS = 8
